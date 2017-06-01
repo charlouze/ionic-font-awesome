8da56eb5495a84eb1e72fce8474c3b01353e7efc
Charlouze <me@charlouze.com>
2017-06-01 10:58:03 +0200

    Add a fa-icon component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import {BrowserModule} from "@angular/platform-browser";
+import {ErrorHandler, NgModule} from "@angular/core";
+import {IonicApp, IonicErrorHandler, IonicModule} from "ionic-angular";
+import {SplashScreen} from "@ionic-native/splash-screen";
+import {StatusBar} from "@ionic-native/status-bar";
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import {MyApp} from "./app.component";
+import {HomePage} from "../pages/home/home";
+import {FaIconComponent} from "../components/fa-icon/fa-icon.component";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    FaIconComponent
   ],
   imports: [
     BrowserModule,
