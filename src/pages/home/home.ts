e7103b018b1e66f9bd3e1187ffb19f72e48ac90d
Charlouze <me@charlouze.com>
2017-06-01 10:58:36 +0200

    Demo HomePage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,42 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from "@angular/core";
+import {NavController} from "ionic-angular";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  static ICONS: string[] = ["camera-retro", "heartbeat", "cutlery"]
+  static COLORS: string[] = ["primary", "secondary", "danger", "light"]
+  static SIZES: string[] = ["lg", "2x", "3x", "4x", "5x"]
 
-  constructor(public navCtrl: NavController) {
+  iconIdx: number = 0;
+  colorIdx: number = 0;
+  sizeIdx: number = 0;
 
+  get icon(): string {
+    return HomePage.ICONS[this.iconIdx];
   }
 
+  get color(): string {
+    return HomePage.COLORS[this.colorIdx];
+  }
+
+  get size(): string {
+    return HomePage.SIZES[this.sizeIdx];
+  }
+
+  constructor(public navCtrl: NavController) {
+    setInterval(() => {
+      if (++this.iconIdx == HomePage.ICONS.length) {
+        this.iconIdx = 0;
+      }
+      if (++this.colorIdx == HomePage.COLORS.length) {
+        this.colorIdx = 0;
+      }
+      if (++this.sizeIdx == HomePage.SIZES.length) {
+        this.sizeIdx = 0;
+      }
+    }, 1000);
+  }
 }
